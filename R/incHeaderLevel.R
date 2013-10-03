47912feb5d3d9e948b62e8ea16c5175b4576ac07
Rainer M. Krug <R.M.Krug@gmail.com>
2013-10-03 11:45:38 +0200

    Fixes documentation
    - documentation fixed
    - Release 0.1.0

--- R/incHeaderLevel.R
+++ R/incHeaderLevel.R
@@ -1,13 +1,15 @@
 ## [[file:~/Documents/Projects/AlienManagementDrakensberg/sim/packages/logger/logger.org::*.incHeaderLevel][\.incHeaderLevel:1]]
-  ##' Increment header level
-  ##'
-  ##' Increases the header level by one, using the given header and clock time
-  ##' @title incHeaderLevel
-  ##' @param header character string, Header of the new header level
-  ##' @param clock character string, containing the timestamp
-  ##' @return  integer value for the the new header level
-  ##' @author Dr Rainer M Krug
-  .incHeaderLevel <- function(header, clock) {
+##' Increment header level
+##'
+##' Increases the header level by one, using the given header and clock time
+##' @title incHeaderLevel
+##' @param header character string, Header of the new header level
+##' @param clock character string, containing the timestamp
+##' @return  integer value for the the new header level
+##' @name incHeaderLevel
+##' @aliases .incHeaderLevel
+##' @author Dr Rainer M Krug
+.incHeaderLevel <- function(header, clock) {
     nl <- getHeaderLevel() + 1
     nh <- c(get("logHeader", envir = .logData), header)
     nc <- c(get("logHeaderClock", envir = .logData), clock)
@@ -15,7 +17,7 @@
     assign("logHeader", nh, envir = .logData)
     assign("logHeaderClock", nc, envir = .logData)
     invisible( nl )
-  }
+}
 ## \.incHeaderLevel:1 ends here
 
 ## Local Variables:
---
+++ R/package.R
@@ -0,0 +1,23 @@
+##' Simple logger
+##'
+##' Simple logger, logging using org-mode style headers and timestamps
+##' 
+##' \tabular{ll}{
+##' Package: \tab logger\cr
+##' Type: \tab Package\cr
+##' Version: \tab 0.0.1\cr
+##' Date: \tab 2013-10-03_11-40\cr
+##' License: \tab GPL (>= 2)\cr
+##' LazyLoad: \tab yes\cr
+##' }
+##'
+##' @name logger-package
+##' @docType package
+##' @author Rainer M Krug \email{Rainer@@krugs.de}
+NULL
+
+## Local Variables:
+## org-babel-tangled-file: t
+## buffer-read-only: t
+## eval:: (auto-revert-mode)
+## End:
